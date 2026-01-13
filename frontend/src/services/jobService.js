165791f879ce02b20d5622e667a364b30709ebf1
nitin-175 <nitinkumarmaurya457@gmail.com>
2026-01-13 10:24:58 +0530

    backend added

---
+++ frontend/src/services/jobService.js
@@ -0,0 +1,17 @@
+
+
+const API_URL = "http://localhost:8080/api/jobs";
+
+export const getJobs = async () => {
+  const res = await fetch(API_URL);
+  return res.json();
+};
+
+export const addJob = async (job) => {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(job),
+  });
+  return res.json();
+};
