68281b6e18332804429ff25c653bed8492ec7514
nitin-175 <nitinkumarmaurya457@gmail.com>
2026-01-23 11:17:44 +0530

    backend files created

---
+++ backend/jobtracker/src/main/java/com/nitin/jobtracker/model/Job.java
@@ -0,0 +1,67 @@
+package com.nitin.jobtracker.model;
+
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "jobs")
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String company;
+    private String role;
+    private String status;
+    private LocalDate appliedDate;
+    private String jobLink;
+
+    public Job() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getAppliedDate() {
+        return appliedDate;
+    }
+
+    public void setAppliedDate(LocalDate appliedDate) {
+        this.appliedDate = appliedDate;
+    }
+
+    public String getJobLink() {
+        return jobLink;
+    }
+
+    public void setJobLink(String jobLink) {
+        this.jobLink = jobLink;
+    }
+}
+
